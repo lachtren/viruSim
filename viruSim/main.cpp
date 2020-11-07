69dd2662304d6e37ab7509dd23fa01f1f4616f52
alanb43 <ab43work@gmail.com>
2020-11-07 13:27:29 -0500

    Removed faulty slider files

--- viruSim/main.cpp
+++ viruSim/main.cpp
@@ -1,11 +1,43 @@
 #include "Sim.h"
+#include "SliderSFML.h"
 
 Sim* Sim::instance = 0;
 
 int main()
 {
-	Sim* sim = sim->getInstance();
-	sim->setup();
-	sim->begin();
+	sf::RenderWindow window(sf::VideoMode(500, 500), "Slider!");
+	window.setFramerateLimit(60);
+
+	SliderSFML slider1(100, 100);
+	SliderSFML slider2(100, 200);
+	SliderSFML slider3(100, 300);
+
+	slider1.create(20, 450);
+	slider2.create(0, 200);
+	slider3.create(0, 100);
+
+	slider1.setSliderValue(235);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear(sf::Color(25, 29, 33));
+
+		slider1.draw(window);
+		slider2.draw(window);
+		slider3.draw(window);
+
+		window.display();
+	}
+
+	//Sim* sim = sim->getInstance();
+	//sim->setup();
+	//sim->begin();
 	return 0;
 }
